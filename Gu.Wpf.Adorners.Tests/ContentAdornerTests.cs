23866671becedcab8ed79413d5ac7cf1ef278df8
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-27 00:44:39 +0100

    Threw in a couple of tests.

---
+++ Gu.Wpf.Adorners.Tests/ContentAdornerTests.cs
@@ -0,0 +1,32 @@
+namespace Gu.Wpf.Adorners.Tests
+{
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Controls;
+    using NUnit.Framework;
+
+    [RequiresSTA]
+    public class ContentAdornerTests
+    {
+        private static readonly MethodInfo MeasureOverrideMethod = typeof(ContentAdorner).GetMethod("MeasureOverride", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly MethodInfo ArrangeOverrideMethod = typeof(ContentAdorner).GetMethod("ArrangeOverride", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        [Test]
+        public void MeasureOverrideWhenChildIsNull()
+        {
+            var textBox = new TextBox();
+            var adorner = new ContentAdorner(textBox) { Child = null };
+            var result = MeasureOverrideMethod.Invoke(adorner, new object[] { new Size(0, 0) });
+            Assert.AreEqual(new Size(0, 0), result);
+        }
+
+        [Test]
+        public void ArrangeOverrideWhenChildIsNull()
+        {
+            var textBox = new TextBox();
+            var adorner = new ContentAdorner(textBox) { Child = null };
+            var result = ArrangeOverrideMethod.Invoke(adorner, new object[] { new Size(0, 0) });
+            Assert.AreEqual(new Size(0, 0), result);
+        }
+    }
+}
---
+++ Gu.Wpf.Adorners.Tests/WatermarkAdornerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Gu.Wpf.Adorners.Tests
+{
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Controls;
+    using NUnit.Framework;
+
+    [RequiresSTA]
+    public class WatermarkAdornerTests
+    {
+        private static readonly MethodInfo MeasureOverrideMethod = typeof(WatermarkAdorner).GetMethod("MeasureOverride", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly MethodInfo ArrangeOverrideMethod = typeof(WatermarkAdorner).GetMethod("ArrangeOverride", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        [Test]
+        public void MeasureOverrideWhenChildIsNull()
+        {
+            var textBox = new TextBox();
+            var adorner = new WatermarkAdorner(textBox) {Child = null};
+            var result = MeasureOverrideMethod.Invoke(adorner, new object[] { new Size(0, 0) });
+            Assert.AreEqual(new Size(0, 0), result);
+        }
+
+        [Test]
+        public void ArrangeOverrideWhenChildIsNull()
+        {
+            var textBox = new TextBox();
+            var adorner = new WatermarkAdorner(textBox) { Child = null };
+            var result = ArrangeOverrideMethod.Invoke(adorner, new object[] { new Size(0, 0) });
+            Assert.AreEqual(new Size(0, 0), result);
+        }
+    }
+}
