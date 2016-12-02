52b31b4958159e98863388f783bdfc157e13eefc
Johan Larsson <johan_gavle1@hotmail.com>
2016-12-02 22:36:38 +0100

    Sample using overlay on PasswordBox

---
+++ Gu.Wpf.Adorners.Demo/IsNotFocusedAndEmptyToTrueConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Gu.Wpf.Adorners.Demo
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class IsNotFocusedAndEmptyToTrueConverter : IMultiValueConverter
+    {
+        public static readonly IsNotFocusedAndEmptyToTrueConverter Default = new IsNotFocusedAndEmptyToTrueConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(values[0], false) && string.IsNullOrEmpty((string) values[1]);
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gu.Wpf.Adorners.Demo/PasswordView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Gu.Wpf.Adorners.Demo
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for PasswordView.xaml
+    /// </summary>
+    public partial class PasswordView : UserControl
+    {
+        public PasswordView()
+        {
+            InitializeComponent();
+        }
+    }
+}
