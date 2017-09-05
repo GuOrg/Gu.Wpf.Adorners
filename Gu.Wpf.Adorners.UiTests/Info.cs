7c58d4fe3b77df396f6b42ed6b69502af32e9941
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-05 09:44:28 +0200

    UI-test project.

---
+++ Gu.Wpf.Adorners.UiTests/Info.cs
@@ -0,0 +1,24 @@
+﻿namespace Gu.Wpf.Adorners.UiTests
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+
+    public static class Info
+    {
+        public static string ExeFileName { get; } = GetExeFileName();
+
+        private static string GetExeFileName()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var testDirectory = Path.GetDirectoryName(new Uri(assembly.CodeBase).LocalPath);
+            var assemblyName = assembly.GetName().Name;
+            var exeDirectoryName = assemblyName.Replace("UiTests", "Demo");
+            //// ReSharper disable once PossibleNullReferenceException
+            var exeDirectory = testDirectory.Replace(assemblyName, exeDirectoryName);
+            var fileName = Path.Combine(exeDirectory, "Gu.Wpf.Adorners.Demo.exe");
+            return fileName;
+        }
+    }
+}
---
+++ Gu.Wpf.Adorners.UiTests/MainWindowTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Gu.Wpf.Adorners.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class MainWindowTests
+    {
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so that everything builds.
+            using (var app = Application.Launch(Info.ExeFileName))
+            {
+                var window = app.MainWindow;
+                var tab = window.FindTabControl();
+                foreach (var tabItem in tab.Items)
+                {
+                    tabItem.Click();
+                }
+            }
+        }
+    }
+}
