b049c3d8d8781196bee39fd2448d94478f6ba90a
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-30 12:56:14 +0200

    Cleanup.

--- Gu.Wpf.Adorners.UiTests/Info.cs
+++ Gu.Wpf.Adorners.UiTests/Info.cs
@@ -1,23 +1,9 @@
 ﻿namespace Gu.Wpf.Adorners.UiTests
 {
-    using System;
-    using System.IO;
-    using System.Reflection;
+    using Gu.Wpf.UiAutomation;
 
     public static class Info
     {
-        public static string ExeFileName { get; } = GetExeFileName();
-
-        private static string GetExeFileName()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var testDirectory = Path.GetDirectoryName(new Uri(assembly.CodeBase).LocalPath);
-            var assemblyName = assembly.GetName().Name;
-            var exeDirectoryName = assemblyName.Replace("UiTests", "Demo");
-            //// ReSharper disable once PossibleNullReferenceException
-            var exeDirectory = testDirectory.Replace(assemblyName, exeDirectoryName);
-            var fileName = Path.Combine(exeDirectory, "Gu.Wpf.Adorners.Demo.exe");
-            return fileName;
-        }
+        public static string ExeFileName { get; } = Application.FindExe("Gu.Wpf.Adorners.Demo.exe");
     }
 }
