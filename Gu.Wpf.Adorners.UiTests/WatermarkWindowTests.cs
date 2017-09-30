40c4e8478981f6ef1bb42b3c042afe64fccb4897
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-30 18:55:45 +0200

    Try ImageAssert

---
+++ Gu.Wpf.Adorners.UiTests/WatermarkWindowTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Gu.Wpf.Adorners.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class WatermarkWindowTests
+    {
+        private const string WindowName = "WatermarkWindow";
+
+        [Test]
+        public void DefaultAdornerWhenNotFocused()
+        {
+            using (var app = Application.Launch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                AutomationElement textBox = window.FindTextBox("WithDefaultAdorner");
+                ImageAssert.AreEqual(".\\Images\\WithDefaultAdorner_not_focused.png", textBox);
+            }
+        }
+    }
+}
