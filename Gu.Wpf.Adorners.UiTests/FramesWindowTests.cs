46d02e92747f2dbf49305b90f2f27a301a290bb0
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-01 11:32:23 +0200

    UI-test for #5.

---
+++ Gu.Wpf.Adorners.UiTests/FramesWindowTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Gu.Wpf.Adorners.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class FramesWindowTests
+    {
+        private const string WindowName = "NotShowingWhenInFrames/FramesWindow";
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            ImageAssert.OnFail = OnFail.SaveImageToTemp;
+        }
+
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so we don't crash.
+            using (var app = Application.Launch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var tab = window.FindTabControl();
+                ImageAssert.AreEqual(".\\Images\\Tab1.png", tab.FindTextBox("Tab1"));
+
+                tab.SelectedIndex = 1;
+                ImageAssert.AreEqual(".\\Images\\Tab2.png", tab.FindTextBox("Tab2"));
+
+                tab.SelectedIndex = 0;
+                ImageAssert.AreEqual(".\\Images\\Tab1.png", tab.FindTextBox("Tab1"));
+
+                tab.SelectedIndex = 1;
+                ImageAssert.AreEqual(".\\Images\\Tab2.png", tab.FindTextBox("Tab2"));
+            }
+        }
+    }
+}
