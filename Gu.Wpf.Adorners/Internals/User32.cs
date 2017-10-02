80d64a3c245787c6fe6345e8e4c22ad82f3e5edb
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-02 14:02:08 +0200

    Formatting.

--- Gu.Wpf.Adorners/Internals/User32.cs
+++ Gu.Wpf.Adorners/Internals/User32.cs
@@ -4,30 +4,30 @@
     using System.Windows;
     using System.Windows.Media;
 
-    public class User32
+    internal class User32
     {
-        [StructLayout(LayoutKind.Sequential)]
-        private struct Win32Point
-        {
-            public int X;
-            public int Y;
-        }
-
-        [DllImport("user32.dll")]
-        private static extern bool GetCursorPos(ref Win32Point pt);
-
-        public static Point GetMousePosition()
+        internal static Point GetMousePosition()
         {
             var p = default(Win32Point);
             GetCursorPos(ref p);
             return new Point(p.X, p.Y);
         }
 
-        public static Point GetMousePosition(Visual relativeTo)
+        internal static Point GetMousePosition(Visual relativeTo)
         {
             var p = default(Win32Point);
             GetCursorPos(ref p);
             return relativeTo.PointFromScreen(new Point(p.X, p.Y));
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Win32Point
+        {
+            public int X;
+            public int Y;
         }
     }
 }
