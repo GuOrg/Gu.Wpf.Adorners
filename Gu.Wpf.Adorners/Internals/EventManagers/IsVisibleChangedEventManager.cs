2fab98c488119126de2ef479fc5a0d749f450319
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-02 17:33:18 +0200

    Weak event managers.

---
+++ Gu.Wpf.Adorners/Internals/EventManagers/IsVisibleChangedEventManager.cs
@@ -0,0 +1,111 @@
+﻿namespace Gu.Wpf.Adorners
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Manager for the DependencyObject.IsVisibleChanged event.
+    /// </summary>
+    internal class IsVisibleChangedEventManager : WeakEventManager
+    {
+        private IsVisibleChangedEventManager()
+        {
+        }
+
+        // get the event manager for the current thread
+        private static IsVisibleChangedEventManager CurrentManager
+        {
+            get
+            {
+                var managerType = typeof(IsVisibleChangedEventManager);
+                var manager = (IsVisibleChangedEventManager)GetCurrentManager(managerType);
+
+                // at first use, create and register a new manager
+                if (manager == null)
+                {
+                    manager = new IsVisibleChangedEventManager();
+                    SetCurrentManager(managerType, manager);
+                }
+
+                return manager;
+            }
+        }
+
+        /// <summary>
+        /// Add a listener to the given source's event.
+        /// </summary>
+        public static void AddListener(FrameworkElement source, IWeakEventListener listener)
+        {
+            CurrentManager.ProtectedAddListener(
+                source ?? throw new ArgumentNullException(nameof(source)),
+                listener ?? throw new ArgumentNullException(nameof(listener)));
+        }
+
+        /// <summary>
+        /// Remove a listener to the given source's event.
+        /// </summary>
+        public static void RemoveListener(FrameworkElement source, IWeakEventListener listener)
+        {
+            CurrentManager.ProtectedRemoveListener(
+                source ?? throw new ArgumentNullException(nameof(source)),
+                listener ?? throw new ArgumentNullException(nameof(listener)));
+        }
+
+        /// <summary>
+        /// Add a handler for the given source's event.
+        /// </summary>
+        public static void AddHandler(FrameworkElement source, EventHandler handler)
+        {
+            CurrentManager.ProtectedAddHandler(
+                source,
+                handler ?? throw new ArgumentNullException(nameof(handler)));
+        }
+
+        /// <summary>
+        /// Remove a handler for the given source's event.
+        /// </summary>
+        public static void RemoveHandler(FrameworkElement source, EventHandler handler)
+        {
+            CurrentManager.ProtectedRemoveHandler(
+                source,
+                handler ?? throw new ArgumentNullException(nameof(handler)));
+        }
+
+        /// <summary>
+        /// Return a new list to hold listeners to the event.
+        /// </summary>
+        protected override ListenerList NewListenerList() => new ListenerList<EventArgs>();
+
+        /// <inheritdoc />
+        protected override void StartListening(object source)
+        {
+            if (source is FrameworkElement fe)
+            {
+                fe.IsVisibleChanged += this.OnIsVisibleChanged;
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot start listening to {source?.GetType().Name ?? "null"}");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void StopListening(object source)
+        {
+            if (source is FrameworkElement fe)
+            {
+                fe.IsVisibleChanged -= this.OnIsVisibleChanged;
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot stop listening to {source?.GetType().Name ?? "null"}");
+            }
+        }
+
+        // event handler for IsVisibleChanged event
+        private void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            this.DeliverEvent(sender, EventArgs.Empty);
+        }
+    }
+}
---
+++ Gu.Wpf.Adorners/Internals/EventManagers/SizeChangedEventManager.cs
@@ -0,0 +1,109 @@
+﻿namespace Gu.Wpf.Adorners
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Manager for the DependencyObject.SizeChanged event.
+    /// </summary>
+    internal class SizeChangedEventManager : WeakEventManager
+    {
+        private SizeChangedEventManager()
+        {
+        }
+
+        // get the event manager for the current thread
+        private static SizeChangedEventManager CurrentManager
+        {
+            get
+            {
+                var managerType = typeof(SizeChangedEventManager);
+                var manager = (SizeChangedEventManager)GetCurrentManager(managerType);
+
+                // at first use, create and register a new manager
+                if (manager == null)
+                {
+                    manager = new SizeChangedEventManager();
+                    SetCurrentManager(managerType, manager);
+                }
+
+                return manager;
+            }
+        }
+
+        /// <summary>
+        /// Add a listener to the given source's event.
+        /// </summary>
+        public static void AddListener(FrameworkElement source, IWeakEventListener listener)
+        {
+            CurrentManager.ProtectedAddListener(
+                source ?? throw new ArgumentNullException(nameof(source)),
+                listener ?? throw new ArgumentNullException(nameof(listener)));
+        }
+
+        /// <summary>
+        /// Remove a listener to the given source's event.
+        /// </summary>
+        public static void RemoveListener(FrameworkElement source, IWeakEventListener listener)
+        {
+            CurrentManager.ProtectedRemoveListener(
+                source ?? throw new ArgumentNullException(nameof(source)),
+                listener ?? throw new ArgumentNullException(nameof(listener)));
+        }
+
+        /// <summary>
+        /// Add a handler for the given source's event.
+        /// </summary>
+        public static void AddHandler(FrameworkElement source, EventHandler<RoutedEventArgs> handler)
+        {
+            CurrentManager.ProtectedAddHandler(
+                source,
+                handler ?? throw new ArgumentNullException(nameof(handler)));
+        }
+
+        /// <summary>
+        /// Remove a handler for the given source's event.
+        /// </summary>
+        public static void RemoveHandler(FrameworkElement source, EventHandler<RoutedEventArgs> handler)
+        {
+            CurrentManager.ProtectedRemoveHandler(
+                source,
+                handler ?? throw new ArgumentNullException(nameof(handler)));
+        }
+
+        /// <inheritdoc />
+        protected override ListenerList NewListenerList() => new ListenerList<RoutedEventArgs>();
+
+        /// <inheritdoc />
+        protected override void StartListening(object source)
+        {
+            if (source is FrameworkElement fe)
+            {
+                fe.SizeChanged += this.OnSizeChanged;
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot start listening to {source?.GetType().Name ?? "null"}");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void StopListening(object source)
+        {
+            if (source is FrameworkElement fe)
+            {
+                fe.SizeChanged -= this.OnSizeChanged;
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot stop listening to {source?.GetType().Name ?? "null"}");
+            }
+        }
+
+        // event handler for SizeChanged event
+        private void OnSizeChanged(object sender, RoutedEventArgs args)
+        {
+            this.DeliverEvent(sender, args);
+        }
+    }
+}
