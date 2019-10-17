35270474416f4145d8007ea7236a907cfbc4a47e
Johan Larsson <johan_gavle1@hotmail.com>
2019-10-18 00:23:40 +0200

    Use new csproj.

---
+++ Gu.Wpf.Adorners.Demo/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
