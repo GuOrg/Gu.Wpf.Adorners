7805dc766962f484cade4d6d3b50d15d732891ff
Johan Larsson <johan_gavle1@hotmail.com>
2019-10-18 18:49:35 +0200

    Use nuget pack.

---
+++ Gu.Wpf.Adorners/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("http://gu.se/Adorners", "Gu.Wpf.Adorners")]
+[assembly: XmlnsPrefix("http://gu.se/Adorners", "adorners")]
