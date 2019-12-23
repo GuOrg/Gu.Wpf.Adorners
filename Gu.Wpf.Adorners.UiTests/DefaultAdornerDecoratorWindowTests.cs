2dd1e0023ab188a827468e36bed03721d7d564fd
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-23 13:04:48 +0100

    DefaultAdornerDecoratorWindowTests

---
+++ Gu.Wpf.Adorners.UiTests/DefaultAdornerDecoratorWindowTests.cs
@@ -0,0 +1,36 @@
+namespace Gu.Wpf.Adorners.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class DefaultAdornerDecoratorWindowTests
+    {
+        private const string ExeFileName = "Gu.Wpf.Adorners.Demo.exe";
+        private const string WindowName = "DefaultAdornerDecoratorWindow";
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // Close the shared window after the last test.
+            Application.KillLaunched(ExeFileName, WindowName);
+        }
+
+        [Test]
+        public void DefaultComboBoxWatermark()
+        {
+            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
+            var window = app.MainWindow;
+            var comboBox = window.FindComboBox("Default");
+            ImageAssert.AreEqual($"Images\\ComboBoxWindow\\{TestImage.CurrentFolder}\\Default.png", comboBox, TestImage.OnFail);
+        }
+
+        [Test]
+        public void DefaultEditableComboBoxWatermark()
+        {
+            using var app = Application.AttachOrLaunch(ExeFileName, WindowName);
+            var window = app.MainWindow;
+            var comboBox = window.FindComboBox("DefaultEditable");
+            ImageAssert.AreEqual($"Images\\ComboBoxWindow\\{TestImage.CurrentFolder}\\Default_editable.png", comboBox, TestImage.OnFail);
+        }
+    }
+}
