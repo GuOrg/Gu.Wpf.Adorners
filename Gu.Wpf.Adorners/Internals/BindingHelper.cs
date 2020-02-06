499649e2eaaaf0905414c8f1eb78a79a381dc129
JohanLarsson <johan_gavle1@hotmail.com>
2020-02-06 08:43:08 +0100

    Remove unused.

--- Gu.Wpf.Adorners/Internals/BindingHelper.cs
+++ Gu.Wpf.Adorners/Internals/BindingHelper.cs
@@ -1,7 +1,6 @@
 namespace Gu.Wpf.Adorners
 {
     using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Data;
 
@@ -16,22 +15,6 @@ namespace Gu.Wpf.Adorners
             return new BindingBuilder(target, targetProperty);
         }
 
-        private static BindingExpression Bind(
-            DependencyObject target,
-            DependencyProperty targetProperty,
-            object source,
-            PropertyPath path)
-        {
-            var binding = new Binding
-            {
-                Path = path,
-                Source = source,
-                Mode = BindingMode.OneWay,
-                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
-            };
-            return (BindingExpression)BindingOperations.SetBinding(target, targetProperty, binding);
-        }
-
         private static PropertyPath GetPath(DependencyProperty property)
         {
             if (PropertyPaths.TryGetValue(property, out PropertyPath? path))
@@ -42,16 +25,6 @@ namespace Gu.Wpf.Adorners
             path = new PropertyPath(property);
             PropertyPaths[property] = path;
             return path;
-        }
-
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        private static BindingExpression Bind(
-            DependencyObject target,
-            DependencyProperty targetProperty,
-            object source,
-            DependencyProperty sourceProperty)
-        {
-            return Bind(target, targetProperty, source, GetPath(sourceProperty));
         }
 
         internal struct BindingBuilder
@@ -69,31 +42,6 @@ namespace Gu.Wpf.Adorners
             {
                 var sourcePath = GetPath(sourceProperty);
                 return this.OneWayTo(source, sourcePath);
-            }
-
-            internal BindingExpression TwoWayTo(object source, DependencyProperty sourceProperty)
-            {
-                var sourcePath = GetPath(sourceProperty);
-                var binding = new Binding
-                {
-                    Source = source,
-                    Path = sourcePath,
-                    Mode = BindingMode.TwoWay,
-                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
-                };
-
-                return (BindingExpression)BindingOperations.SetBinding(this.target, this.targetProperty, binding);
-            }
-
-            internal BindingExpression OneWayTo(object source)
-            {
-                var binding = new Binding
-                {
-                    Source = source,
-                    Mode = BindingMode.OneWay,
-                };
-
-                return (BindingExpression)BindingOperations.SetBinding(this.target, this.targetProperty, binding);
             }
 
             internal BindingExpression OneWayTo(object source, PropertyPath sourcePath)
