23c25be879d3c8306e93df939daf01879b93c349
Johan Larsson <johan_gavle1@hotmail.com>
2021-01-02 01:28:19 +0100

    [assembly: CLSCompliant(false)]

--- Gu.Wpf.Adorners/AssemblyAttributes.cs
+++ Gu.Wpf.Adorners/AssemblyAttributes.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly:ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: CLSCompliant(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://gu.se/Adorners", "Gu.Wpf.Adorners")]
 [assembly: XmlnsPrefix("http://gu.se/Adorners", "adorners")]
